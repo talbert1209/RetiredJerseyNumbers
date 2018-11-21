51ad92c5b3284587586c6605f0c7baedab5cb5e0
talbert1209 <talbert1209@gmail.com>
2018-11-21 14:35:08 -0600

    Built simple app

--- RetiredJerseyNumbers/Form1.Designer.cs
+++ RetiredJerseyNumbers/Form1.Designer.cs
@@ -28,13 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.numberLabel = new System.Windows.Forms.Label();
+            this.numberComboBox = new System.Windows.Forms.ComboBox();
+            this.playerLabel = new System.Windows.Forms.Label();
+            this.playerTextBox = new System.Windows.Forms.TextBox();
+            this.yearLabel = new System.Windows.Forms.Label();
+            this.yearTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.Location = new System.Drawing.Point(35, 33);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(58, 17);
+            this.numberLabel.TabIndex = 0;
+            this.numberLabel.Text = "Number";
+            // 
+            // numberComboBox
+            // 
+            this.numberComboBox.FormattingEnabled = true;
+            this.numberComboBox.Location = new System.Drawing.Point(99, 30);
+            this.numberComboBox.Name = "numberComboBox";
+            this.numberComboBox.Size = new System.Drawing.Size(121, 24);
+            this.numberComboBox.TabIndex = 1;
+            this.numberComboBox.SelectedIndexChanged += new System.EventHandler(this.numberComboBox_SelectedIndexChanged);
+            // 
+            // playerLabel
+            // 
+            this.playerLabel.AutoSize = true;
+            this.playerLabel.Location = new System.Drawing.Point(227, 33);
+            this.playerLabel.Name = "playerLabel";
+            this.playerLabel.Size = new System.Drawing.Size(85, 17);
+            this.playerLabel.TabIndex = 2;
+            this.playerLabel.Text = "was worn by";
+            // 
+            // playerTextBox
+            // 
+            this.playerTextBox.Location = new System.Drawing.Point(318, 30);
+            this.playerTextBox.Name = "playerTextBox";
+            this.playerTextBox.Size = new System.Drawing.Size(100, 22);
+            this.playerTextBox.TabIndex = 3;
+            // 
+            // yearLabel
+            // 
+            this.yearLabel.AutoSize = true;
+            this.yearLabel.Location = new System.Drawing.Point(424, 33);
+            this.yearLabel.Name = "yearLabel";
+            this.yearLabel.Size = new System.Drawing.Size(92, 17);
+            this.yearLabel.TabIndex = 4;
+            this.yearLabel.Text = "and retired in";
+            // 
+            // yearTextBox
+            // 
+            this.yearTextBox.Location = new System.Drawing.Point(522, 30);
+            this.yearTextBox.Name = "yearTextBox";
+            this.yearTextBox.Size = new System.Drawing.Size(100, 22);
+            this.yearTextBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(659, 81);
+            this.Controls.Add(this.yearTextBox);
+            this.Controls.Add(this.yearLabel);
+            this.Controls.Add(this.playerTextBox);
+            this.Controls.Add(this.playerLabel);
+            this.Controls.Add(this.numberComboBox);
+            this.Controls.Add(this.numberLabel);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label numberLabel;
+        private System.Windows.Forms.ComboBox numberComboBox;
+        private System.Windows.Forms.Label playerLabel;
+        private System.Windows.Forms.TextBox playerTextBox;
+        private System.Windows.Forms.Label yearLabel;
+        private System.Windows.Forms.TextBox yearTextBox;
     }
 }
 
---
+++ RetiredJerseyNumbers/JerseyNumber.cs
@@ -0,0 +1,19 @@
+﻿namespace RetiredJerseyNumbers
+{
+    public class JerseyNumber
+    {
+        public string Player { get; private set; }
+        public int YearRetired { get; private set; }
+
+        public JerseyNumber(string player, int year)
+        {
+            Player = player;
+            YearRetired = year;
+        }
+
+        public override string ToString()
+        {
+            return Player;
+        }
+    }
+}
